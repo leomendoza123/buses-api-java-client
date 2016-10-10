1ea981f16a80af329c4a461708a030c3fb42777b
Leonardo <leomendoza123@gmail.com>
2016-10-09 21:31:44 -0600

    maven init

---
+++ labexp-traces-sdk/src/main/java/org/labexp/traces/App.java
@@ -0,0 +1,13 @@
+package org.labexp.traces;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
